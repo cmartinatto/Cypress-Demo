7f52a55f2f9160ff07d02a2477092cb2e946ceae
carlos-martinatto <cmartinatto@gmail.com>
2026-04-09 15:47:34 -0300

    change some TC id

--- cypress/e2e/api/users.cy.ts
+++ cypress/e2e/api/users.cy.ts
@@ -297,7 +297,7 @@ describe("API - Users", () => {
 
     it(
       "should return responseCode 405 when using GET instead of DELETE",
-      { tags: ["TC-API-324"] },
+      { tags: ["TC-API-424"] },
       () => {
         cy.request({
           method: "GET",
