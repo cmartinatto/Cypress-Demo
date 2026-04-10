d63780440e79924d8a33a4ceb597d32a89d020e4
carlos martinatto <carlos.martinatto@devlane.com>
2026-04-10 08:20:05 -0300

    change test case id

--- cypress/e2e/api/users.cy.ts
+++ cypress/e2e/api/users.cy.ts
@@ -297,7 +297,7 @@ describe("API - Users", () => {
 
     it(
       "should return responseCode 405 when using GET instead of DELETE",
-      { tags: ["TC-API-424"] },
+      { tags: ["TC-API-524"] },
       () => {
         cy.request({
           method: "GET",
