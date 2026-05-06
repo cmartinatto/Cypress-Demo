d3428ac0dac30ab71a9cf70194ec380ee527ca22
carlos-martinatto <cmartinatto@gmail.com>
2026-05-06 16:32:23 -0300

    fix format issues

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,7 +24,6 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (Cypress.Commands.overwrite as any)(
   "type",
